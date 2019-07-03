815106634c82d03be5d76250296734b884762877
surest <AR13133@163.com>
2019-07-03 15:49:25 +0800

    增加图片上传 + 店铺状态等

--- src/layout/components/http.js
+++ src/layout/components/http.js
@@ -11,7 +11,7 @@ const http = {}
 // dev 开发版
 // production 生产环境
 // const host = "http://store.ink/admin/"
-// const host = "http://test-offline.d88.ink/admin/"
+const host = "http://test-offline.d88.ink/admin/"
 // const host = "https://offline.d88.ink/admin/"
 // const host = "http://192.168.2.199:96/admin/"
 
