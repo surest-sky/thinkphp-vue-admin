7a0f3af4d3c01837e6739a4e165f2634f3ac37c7
surest <AR13133@163.com>
2019-07-19 11:42:39 +0800

    发布测试环境 - 管理后台

--- src/settings.js
+++ src/settings.js
@@ -15,8 +15,8 @@ module.exports = {
   sidebarLogo: false,
 
   // AdminApiUrl: 'http://store.ink', // 本地
-  // AdminApiUrl: 'http://test-offline.d88.ink/admin', // 线上
-  AdminApiUrl: 'http://192.168.2.199:96/admin', // 周坤
+  AdminApiUrl: 'http://test-offline.d88.ink/admin', // 线上
+  // AdminApiUrl: 'http://192.168.2.199:96/admin', // 周坤
 
     /**
    * @type {boolean} true | false
