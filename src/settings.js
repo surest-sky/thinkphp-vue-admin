0e868ea5d206be4f508b73ab32c924e3ee293890
surest <AR13133@163.com>
2019-07-21 19:59:07 +0800

    --

--- src/settings.js
+++ src/settings.js
@@ -14,8 +14,8 @@ module.exports = {
    */
   sidebarLogo: false,
 
-  AdminApiUrl: 'http://store.ink', // 本地
-  // AdminApiUrl: 'http://test-offline.d88.ink', // 线上
+  // AdminApiUrl: 'http://store.ink', // 本地
+  AdminApiUrl: 'http://test-offline.d88.ink', // 线上
   // AdminApiUrl: 'http://192.168.2.199:96', // 周坤
 
     /**
